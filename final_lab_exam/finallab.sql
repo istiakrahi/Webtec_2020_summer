6e01b5d295932b3a8cb30dc680a16d95b4d5ccdf
istiakrahi <67922668+istiakrahi@users.noreply.github.com>
2020-09-07 13:22:26 +0600

    Add files via upload
    last commit

---
+++ final_lab_exam/finallab.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2020 at 09:22 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `finallab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `empname` varchar(20) NOT NULL,
+  `companyname` varchar(30) DEFAULT NULL,
+  `contactno` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `PASSWORD` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- final_lab_exam/php/userController.php
+++ final_lab_exam/php/userController.php
@@ -12,6 +12,7 @@
 		$contactno  = $_POST['contactno'];
 		$username   = $_POST['username'];
 		$password 	= $_POST['password'];
+		$id  = $_POST['id'];
 
 		if(empty($empname)|| empty($companyname) || empty($contactno) || empty($username) || empty($password)){
 			header('location: ../views/edit.php?id='.$id);
@@ -22,6 +23,7 @@
 				'contactno'  =>$contactno,
 				'username'	=>$username,
 				'password'	=>$password,
+				'id' => $id
 				
 				
 
@@ -29,10 +31,51 @@
 
 			$status = update($user);
 			if($status){
-				header('location: ../views/user_list.php?msg=success');
+				header('location: ../views/employee_list.php?msg=success');
 			}else{
 				header('location: ../views/edit.php?id='.$id);
 			}
 		}	
 	}
+
+
+
+if (isset($_POST['delete'])) {
+	# code...
+	    $empname    = $_POST['empname'];
+		$companyname= $_POST['companyname'];
+		$contactno  = $_POST['contactno'];
+		$username   = $_POST['username'];
+		$password 	= $_POST['password'];
+		$id  = $_POST['id'];
+
+
+if(empty($empname)|| empty($companyname) || empty($contactno) || empty($username) || empty($password))
+	 {
+		header('location: ../views/delete.php?id='.$id);
+
+	}
+	else
+	{
+		$user = [
+			    'empname'   =>$empname,
+				'companyname'=>$companyname,
+				'contactno'  =>$contactno,
+				'username'	=>$username,
+				'password'	=>$password,
+				'id' => $id
+
+		];
+
+		$status = delete($user);
+		if ($status) {
+			header('location: ../views/employee_list.php?msg=success');
+		}
+		else
+		{
+			header('location: ../views/delete.php?id= '.$id);
+		}
+	}
+}
+?>
 ?>
